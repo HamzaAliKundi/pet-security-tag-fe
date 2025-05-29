e85b87812728072f976ff2c4c4df96366853e46b
HamzaAliKundi <hamza.alee83@gmail.com>
2025-05-29 20:21:51 +0500

    button fixes

--- src/components/home/getMoreOnfo.jsx
+++ src/components/home/getMoreOnfo.jsx
@@ -26,6 +26,7 @@ const GetMoreInfo = () => {
           {/* Button with Gradient using Tailwind */}
           <Link
             to="/get-info"
+            onClick={() => window.scrollTo(0, 0)}
             className="w-full max-w-[268px] h-[46px] sm:h-[56px] rounded-full px-6 sm:px-8 py-3 sm:py-4 text-black font-helvetica-neue font-bold text-[12px] sm:text-[14px] leading-[100%] uppercase
             bg-gradient-to-br from-[#FFD700] to-[#B89D0B] hover:-translate-y-0.5 transition-transform duration-200
             shadow-lg hover:shadow-xl active:translate-y-0 active:shadow-md
