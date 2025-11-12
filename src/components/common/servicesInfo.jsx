c95c2a83324bf70c2acd6618dbe69349b7ac4347
HamzaAliKundi <hamza.alee83@gmail.com>
2025-11-12 12:40:24 +0500

    Add tag section

--- src/components/common/servicesInfo.jsx
+++ src/components/common/servicesInfo.jsx
@@ -5,11 +5,10 @@ const ServicesInfo = () => {
     <div className="max-w-9xl mx-auto px-4 py-8 sm:py-12 md:py-16">
       <div className="w-full max-w-[957px] mx-auto bg-[#4CB2E2] rounded-[8px] px-4 sm:px-8 md:px-12 py-4 sm:py-6">
         <h2 className="font-helvetica-neue font-semibold text-[16px] sm:text-[22px] md:text-[20px] text-white text-center sm:text-left mb-2">
-          Setting Up Your PawTag
+         Setting up your Digital Tail smart tag
         </h2>
         <p className="font-helvetica-neue text-[16px]  text-white text-center sm:text-left capitalize">
-          Once Your Tag Arrives, Scan It To Set Up Your Pet's Profile. For Just £2.75/month Or One Payment Of £28.99 Yearly, You Can Activate Your Tag And Access All The Features To Keep Your Pet Safe.
-          Seniors Age 65+ Get Any Package For Free!
+        Once your tag arrives, simply scan it to create your pet’s profile! For just £2.75 per month or £28.99 a year, you’ll unlock all features and keep your pet safe and connected.
         </p>  
       </div>
       
