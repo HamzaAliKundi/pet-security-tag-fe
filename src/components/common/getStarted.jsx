f5da805e8eea6be25c5927b4b12a238bb264b0b7
HamzaAliKundi <hamza.alee83@gmail.com>
2026-02-25 22:28:00 +0500

    fix scrill issue

--- src/components/common/getStarted.jsx
+++ src/components/common/getStarted.jsx
@@ -44,7 +44,7 @@ const GetStarted = () => {
         {steps.map((step, index) => (
           <div key={index} className="relative flex-shrink-0 w-[300px] md:w-auto">
             {/* Step Card */}
-            <div className="w-[300px] sm:w-[320px] md:w-[264px] h-[280px] sm:h-[300px] md:h-[232px] bg-white rounded-[8px] shadow-sm p-4">
+            <div className="w-[300px] sm:w-[320px] md:w-[264px] min-h-[380px] sm:min-h-[400px] md:min-h-[280px] bg-white rounded-[8px] shadow-sm p-4">
               {/* Number Box */}
               <div className="w-[48px] h-[48px] bg-[#FDD30F] rounded-[8px] flex items-center justify-center mb-4">
                 <span className="font-helvetica-neue font-bold text-[18px]">
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -31,7 +31,7 @@ const HomePage = () => {
       {/* <Customers /> */}
       {/* <Reviews /> */}
       <CustomerReviews />
-      {/* <GetStarted /> */}
+      <GetStarted />
       <Order />
       {/* Partner Charities */}
       <Partnership />
