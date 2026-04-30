b0738750fe67adbb5ffe54a2fc3a432c8f5501df
HamzaAliKundi <hamza.alee83@gmail.com>
2026-04-30 19:08:30 +0500

    udpate how it works

--- src/components/home/HowItWorksPoster.jsx
+++ src/components/home/HowItWorksPoster.jsx
@@ -4,9 +4,9 @@ const HowItWorksPoster = () => {
   return (
     <div className="w-full flex justify-center py-8 px-4">
       <img
-        src="/global/how-it-works.png"
+        src="/global/how-it-works-1.png"
         alt="How it works - Digital Tails Pet Security Tag"
-        className="max-w-[500px] w-full h-auto object-contain"
+        className="max-w-[950px] w-full h-auto object-contain"
       />
     </div>
   );
